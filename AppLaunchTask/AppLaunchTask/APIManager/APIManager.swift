7eeb05b2872bb40fbf3e0d72a8670c8c676c6785
Ragini <raginimohan2721997@gmail.com>
2021-06-30 09:43:28 +0530

    commit

--- AppLaunchTask/AppLaunchTask/APIManager/APIManager.swift
+++ AppLaunchTask/AppLaunchTask/APIManager/APIManager.swift
@@ -7,30 +7,30 @@
 
 import Foundation
 import UIKit
-import Alamofire
-
-class APIManager: NSObject {
-
-    class func apiGetWithToken(serviceName:String,parameters: [String:Any]?, completionHandler: @escaping (NSDictionary?, NSError?) -> ()) {
-        print("BASE URL : \(serviceName as String)")
-        print("PARAMETER : \(parameters!)")
-        
-       
-        let postheaders : HTTPHeaders = ["Authorization" : ""]
-        Alamofire.request(serviceName, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: postheaders).responseJSON { (response:DataResponse<Any>) in
-            switch(response.result) {
-            case .success(_):
-                if let data = response.result.value{
-                    let json = data as? NSDictionary
-                    completionHandler(json,nil)
-                }
-                break
-            case .failure(_):
-                completionHandler(nil,response.result.error as NSError?)
-                break
-                
-            }
-        }
-    }
-
-}
+//import Alamofire
+//
+//class APIManager: NSObject {
+//
+//    class func apiGetWithToken(serviceName:String,parameters: [String:Any]?, completionHandler: @escaping (NSDictionary?, NSError?) -> ()) {
+//        print("BASE URL : \(serviceName as String)")
+//        print("PARAMETER : \(parameters!)")
+//
+//
+//        let postheaders : HTTPHeaders = ["Authorization" : ""]
+//        Alamofire.request(serviceName, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: postheaders).responseJSON { (response:DataResponse<Any>) in
+//            switch(response.result) {
+//            case .success(_):
+//                if let data = response.result.value{
+//                    let json = data as? NSDictionary
+//                    completionHandler(json,nil)
+//                }
+//                break
+//            case .failure(_):
+//                completionHandler(nil,response.result.error as NSError?)
+//                break
+//
+//            }
+//        }
+//    }
+//
+//}
--- AppLaunchTask/AppLaunchTask/Weather/WeatherViewController.swift
+++ AppLaunchTask/AppLaunchTask/Weather/WeatherViewController.swift
@@ -12,29 +12,29 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        fetchWeatherReport()
+//        fetchWeatherReport()
         
     }
     
-    func fetchWeatherReport() {
-        
-      
-        let stringURL = "https://api.openweathermap.org/data/2.5/onecall?lat=12.9082847623315&lon=77.65197822993314&units=imperial&appid=b143bb707b2ee117e62649b358207d3e"
-       
-        APIManager.apiGetWithToken(serviceName: stringURL, parameters: nil) { (json:NSDictionary?, error:NSError?) in
-            DispatchQueue.main.async { self.showAnimatedLoader(isAnimated: false) }
-            if error != nil {
-                print(error!.localizedDescription)
-                return
-            }
-            print(json!)
-//            if json?.value(forKey: "status") as! NSNumber == 200 {
-                let reviewsArray:NSArray = json?.value(forKey: "data") as! NSArray
-                print(reviewsArray)
+//    func fetchWeatherReport() {
+//
+//
+//        let stringURL = "https://api.openweathermap.org/data/2.5/onecall?lat=12.9082847623315&lon=77.65197822993314&units=imperial&appid=b143bb707b2ee117e62649b358207d3e"
+//
+//        APIManager.apiGetWithToken(serviceName: stringURL, parameters: nil) { (json:NSDictionary?, error:NSError?) in
+//            DispatchQueue.main.async { self.showAnimatedLoader(isAnimated: false) }
+//            if error != nil {
+//                print(error!.localizedDescription)
+//                return
 //            }
-//            else {
-//                self.showInformation(title: "Torripo", message: json?.value(forKey: "message") as! String)
-//            }
-        }
-    }
+//            print(json!)
+////            if json?.value(forKey: "status") as! NSNumber == 200 {
+//                let reviewsArray:NSArray = json?.value(forKey: "data") as! NSArray
+//                print(reviewsArray)
+////            }
+////            else {
+////                self.showInformation(title: "Torripo", message: json?.value(forKey: "message") as! String)
+////            }
+//        }
+//    }
 }
